3c72961d6ed55e7b12e35d9d1eeaab8f833ba8b7
azizSharopov <sharopovaziz23@gmail.com>
2023-02-24 12:48:11 +0900

    feat: develop homepage related best restaurants section

--- src/app/screens/HomePage/bestRestaurants.tsx
+++ src/app/screens/HomePage/bestRestaurants.tsx
@@ -1,10 +1,383 @@
-import { Container } from "@mui/material";
 import React from "react";
+import { Favorite, Visibility } from "@mui/icons-material";
+import {
+  AspectRatio,
+  Card,
+  CardOverflow,
+  CssVarsProvider,
+  IconButton,
+  Link,
+  Typography,
+} from "@mui/joy";
+import { Box, Container, Stack, Button } from "@mui/material";
+import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
+import CallIcon from "@mui/icons-material/Call";
 
 export function BestRestaurants() {
   return (
-    <div className="top_restaurant_frame">
-      <Container>BestRestaurants</Container>
+    <div className="best_restaurant_frame">
+      <img
+        src={"icons/line_group.svg"}
+        style={{ position: "absolute", left: "6%", transform: "rotate(90deg)" }}
+      />
+      <Container sx={{ mt: "153px" }}>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Box className="category_title">Zo’r Restaurantlar</Box>
+          <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
+            <CssVarsProvider>
+              <Card
+                variant="outlined"
+                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
+              >
+                <CardOverflow>
+                  <AspectRatio ratio="1">
+                    <img src={"restaurant/burak.jpeg"} alt="" />
+                  </AspectRatio>
+                  <IconButton
+                    aria-label="Like minimal photography"
+                    size="md"
+                    variant="solid"
+                    color="neutral"
+                    sx={{
+                      position: "absolute",
+                      zIndex: 2,
+                      borderRadius: "50%",
+                      right: "1rem",
+                      bottom: 0,
+                      transform: "translateY(50%)",
+                      color: "rgba(0,0,0,.4)",
+                    }}
+                  >
+                    <Favorite style={{ color: "white" }} />
+                  </IconButton>
+                </CardOverflow>
+                <Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
+                  Texas De Brazil restaurant
+                </Typography>
+                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                  <Link
+                    href=""
+                    startDecorator={<LocationOnRoundedIcon />}
+                    textColor="neutral.700"
+                  >
+                    Tashkent, Yunus Abad 4-1
+                  </Link>
+                </Typography>
+                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                  <Link
+                    href=""
+                    startDecorator={<CallIcon />}
+                    textColor="neutral.700"
+                  >
+                    +99890 7314578
+                  </Link>
+                </Typography>
+                <CardOverflow
+                  variant="soft"
+                  sx={{
+                    display: "flex",
+                    gap: 1.5,
+                    py: 1.5,
+                    px: "var(--Card-padding)",
+                    borderTop: "1px solid",
+                    borderColor: "neutral.outlinedBorder",
+                    bgcolor: "background.level1",
+                  }}
+                >
+                  <Typography
+                    level="body3"
+                    sx={{
+                      fontWeight: "md",
+                      color: "text.secondary",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    100{""}
+                    <Visibility sx={{ fontSize: 20, marginLeft: "5px" }} />
+                  </Typography>
+                  <Box sx={{ witdh: 2, bgcolor: "divider" }} />
+                  <Typography
+                    sx={{
+                      fontWeight: "md",
+                      color: "text.secondary",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <div>500</div>
+                    <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
+                  </Typography>
+                </CardOverflow>
+              </Card>
+              <Card
+                variant="outlined"
+                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
+              >
+                <CardOverflow>
+                  <AspectRatio ratio="1">
+                    <img src={"restaurant/burak.jpeg"} alt="" />
+                  </AspectRatio>
+                  <IconButton
+                    aria-label="Like minimal photography"
+                    size="md"
+                    variant="solid"
+                    color="neutral"
+                    sx={{
+                      position: "absolute",
+                      zIndex: 2,
+                      borderRadius: "50%",
+                      right: "1rem",
+                      bottom: 0,
+                      transform: "translateY(50%)",
+                      color: "rgba(0,0,0,.4)",
+                    }}
+                  >
+                    <Favorite style={{ color: "white" }} />
+                  </IconButton>
+                </CardOverflow>
+                <Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
+                  Texas De Brazil restaurant
+                </Typography>
+                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                  <Link
+                    href=""
+                    startDecorator={<LocationOnRoundedIcon />}
+                    textColor="neutral.700"
+                  >
+                    Tashkent, Yunus Abad 4-1
+                  </Link>
+                </Typography>
+                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                  <Link
+                    href=""
+                    startDecorator={<CallIcon />}
+                    textColor="neutral.700"
+                  >
+                    +99890 7314578
+                  </Link>
+                </Typography>
+                <CardOverflow
+                  variant="soft"
+                  sx={{
+                    display: "flex",
+                    gap: 1.5,
+                    py: 1.5,
+                    px: "var(--Card-padding)",
+                    borderTop: "1px solid",
+                    borderColor: "neutral.outlinedBorder",
+                    bgcolor: "background.level1",
+                  }}
+                >
+                  <Typography
+                    level="body3"
+                    sx={{
+                      fontWeight: "md",
+                      color: "text.secondary",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    100{""}
+                    <Visibility sx={{ fontSize: 20, marginLeft: "5px" }} />
+                  </Typography>
+                  <Box sx={{ witdh: 2, bgcolor: "divider" }} />
+                  <Typography
+                    sx={{
+                      fontWeight: "md",
+                      color: "text.secondary",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <div>500</div>
+                    <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
+                  </Typography>
+                </CardOverflow>
+              </Card>
+              <Card
+                variant="outlined"
+                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
+              >
+                <CardOverflow>
+                  <AspectRatio ratio="1">
+                    <img src={"restaurant/burak.jpeg"} alt="" />
+                  </AspectRatio>
+                  <IconButton
+                    aria-label="Like minimal photography"
+                    size="md"
+                    variant="solid"
+                    color="neutral"
+                    sx={{
+                      position: "absolute",
+                      zIndex: 2,
+                      borderRadius: "50%",
+                      right: "1rem",
+                      bottom: 0,
+                      transform: "translateY(50%)",
+                      color: "rgba(0,0,0,.4)",
+                    }}
+                  >
+                    <Favorite style={{ color: "white" }} />
+                  </IconButton>
+                </CardOverflow>
+                <Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
+                  Texas De Brazil restaurant
+                </Typography>
+                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                  <Link
+                    href=""
+                    startDecorator={<LocationOnRoundedIcon />}
+                    textColor="neutral.700"
+                  >
+                    Tashkent, Yunus Abad 4-1
+                  </Link>
+                </Typography>
+                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                  <Link
+                    href=""
+                    startDecorator={<CallIcon />}
+                    textColor="neutral.700"
+                  >
+                    +99890 7314578
+                  </Link>
+                </Typography>
+                <CardOverflow
+                  variant="soft"
+                  sx={{
+                    display: "flex",
+                    gap: 1.5,
+                    py: 1.5,
+                    px: "var(--Card-padding)",
+                    borderTop: "1px solid",
+                    borderColor: "neutral.outlinedBorder",
+                    bgcolor: "background.level1",
+                  }}
+                >
+                  <Typography
+                    level="body3"
+                    sx={{
+                      fontWeight: "md",
+                      color: "text.secondary",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    100{""}
+                    <Visibility sx={{ fontSize: 20, marginLeft: "5px" }} />
+                  </Typography>
+                  <Box sx={{ witdh: 2, bgcolor: "divider" }} />
+                  <Typography
+                    sx={{
+                      fontWeight: "md",
+                      color: "text.secondary",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <div>500</div>
+                    <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
+                  </Typography>
+                </CardOverflow>
+              </Card>
+              <Card
+                variant="outlined"
+                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
+              >
+                <CardOverflow>
+                  <AspectRatio ratio="1">
+                    <img src={"restaurant/burak.jpeg"} alt="" />
+                  </AspectRatio>
+                  <IconButton
+                    aria-label="Like minimal photography"
+                    size="md"
+                    variant="solid"
+                    color="neutral"
+                    sx={{
+                      position: "absolute",
+                      zIndex: 2,
+                      borderRadius: "50%",
+                      right: "1rem",
+                      bottom: 0,
+                      transform: "translateY(50%)",
+                      color: "rgba(0,0,0,.4)",
+                    }}
+                  >
+                    <Favorite style={{ color: "white" }} />
+                  </IconButton>
+                </CardOverflow>
+                <Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
+                  Texas De Brazil restaurant
+                </Typography>
+                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                  <Link
+                    href=""
+                    startDecorator={<LocationOnRoundedIcon />}
+                    textColor="neutral.700"
+                  >
+                    Tashkent, Yunus Abad 4-1
+                  </Link>
+                </Typography>
+                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                  <Link
+                    href=""
+                    startDecorator={<CallIcon />}
+                    textColor="neutral.700"
+                  >
+                    +99890 7314578
+                  </Link>
+                </Typography>
+                <CardOverflow
+                  variant="soft"
+                  sx={{
+                    display: "flex",
+                    gap: 1.5,
+                    py: 1.5,
+                    px: "var(--Card-padding)",
+                    borderTop: "1px solid",
+                    borderColor: "neutral.outlinedBorder",
+                    bgcolor: "background.level1",
+                  }}
+                >
+                  <Typography
+                    level="body3"
+                    sx={{
+                      fontWeight: "md",
+                      color: "text.secondary",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    100{""}
+                    <Visibility sx={{ fontSize: 20, marginLeft: "5px" }} />
+                  </Typography>
+                  <Box sx={{ witdh: 2, bgcolor: "divider" }} />
+                  <Typography
+                    sx={{
+                      fontWeight: "md",
+                      color: "text.secondary",
+                      alignItems: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <div>500</div>
+                    <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
+                  </Typography>
+                </CardOverflow>
+              </Card>
+            </CssVarsProvider>
+          </Stack>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"flex-end"}
+            style={{ width: "100%", marginTop: "16px" }}
+          >
+            <Button style={{ background: "#1976d2", color: "#FFFFFF" }}>
+              Barchasini ko'rish
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
